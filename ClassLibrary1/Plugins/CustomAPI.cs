b9fced25c9b58aa7d9a4adb1b65e258c7c937d20
kmevada <kmevada@SYN-009115>
2024-04-29 12:15:32 +0530

    Add project files.

---
+++ ClassLibrary1/Plugins/CustomAPI.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Plugins
+{
+    public class CustomAPI : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IOrganizationService crmService = null;
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ITracingService tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            if (context.MessageName.Equals("new_testapi"))
+            {
+                try
+                {
+                    tracing.Trace("Execution started");
+
+                    IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                    crmService = serviceFactory.CreateOrganizationService(context.UserId);
+
+                    // retrieve the inputs
+                    string accountname = (string)context.InputParameters["accountname"];
+                    string accountId = (string)context.InputParameters["parentaccountid"];
+                    tracing.Trace("accountname : " + accountname);
+                    tracing.Trace("parentaccountid : " + accountId);
+
+                    // create new account from the inputs
+                    Entity createAccount = new Entity("account");
+                    createAccount["name"] = accountname;
+                    createAccount["parentaccountid"] = new EntityReference("account", Guid.Parse(accountId));
+                    tracing.Trace("account created");
+
+                    Guid accId = crmService.Create(createAccount);
+
+                    context.OutputParameters["apiresponse"] = accId.ToString();
+                    tracing.Trace("Execution completed");
+                }
+                catch (Exception)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}
